4f103e66231ccc921e8dbe99081cffb8cc184f1c
hannahrose1593 <hannah.rose1593@gmail.com>
2016-11-15 19:13:52 -0600

    hide and show functions

--- initializr 2/js/main.js
+++ initializr 2/js/main.js
@@ -1,4 +1,14 @@
 function highlight() {
   $('h1').text('Hello');
-  $(".jumbotron").addClass('highlighted');  
+  $(".jumbotron").addClass('highlighted');
+}
+function unhighlight() {
+  $('h1').text('Hello, world!');
+  $(".jumbotron").removeClass('highlighted');
+}
+function hide(){
+  $(".jumbotron").fadeOut();
+}
+function show() {
+  $('.jumbotron').slideDown();
 }
